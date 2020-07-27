1112a69b50910372986ae4e8c2af8722a2591661
曹宇明 <yuming.cao@maibaapp.cn>
2020-07-27 09:48:37 +0800

    Initial Commit

---
+++ WidgetDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WidgetDemo
+//
+//  Created by 曹宇明 on 2020/7/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
